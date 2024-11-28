192c47be13a80ffe4b3536bc59383243ae599212
Grzybski02 <jacek.grzybowski.110920@student.pb.edu.pl>
2024-11-28 18:29:12 +0100

    Implemented double click to change properties & auto-resizing after change of label

---
+++ EditNodeWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Decision_Trees_Visualizer;
+/// <summary>
+/// Logika interakcji dla klasy EditNodeWindow.xaml
+/// </summary>
+public partial class EditNodeWindow : Window
+{
+    public Node Node { get; set; }
+    public List<string> ColorNames { get; set; }
+
+    public EditNodeWindow(Node node, List<string> colorNames)
+    {
+        InitializeComponent();
+        Node = node;
+        ColorNames = colorNames;
+        DataContext = this; // Set DataContext to the window
+    }
+
+    private void OkButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+        Close();
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        Close();
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Msagl.Drawing;
 using Microsoft.Msagl.GraphViewerGdi;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.IO;
 using System.Text;
 using System.Windows;
@@ -13,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Decision_Trees_Visualizer;
 
@@ -31,13 +34,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = this; // Ustawienie DataContext
+        DataContext = this;
 
         grapher = new GrapherSKL();
         Nodes = new ObservableCollection<Node>();
+        Nodes.CollectionChanged += Nodes_CollectionChanged;
         NodeGrid.ItemsSource = Nodes;
 
-        // Inicjalizacja ColorNames za pomocą publicznej metody
         ColorNames = new ColorList().GetPredefinedColorNames();
         var colorColumn = NodeGrid.Columns.OfType<DataGridComboBoxColumn>().FirstOrDefault(c => c.Header.ToString() == "Color");
         if (colorColumn != null)
@@ -73,6 +76,7 @@ public partial class MainWindow : Window
         }
 
         gViewer = grapher.RenderDecisionTree(nodes);
+        gViewer.MouseDoubleClick += GViewer_MouseDoubleClick; // Subscribe to the event
         graphHost.Child = gViewer;
     }
 
@@ -89,11 +93,7 @@ public partial class MainWindow : Window
     {
         if (e.EditAction == DataGridEditAction.Commit)
         {
-            // Defer the update until after the edit is committed
-            this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(() =>
-            {
-                UpdateGraphNode(e);
-            }));
+            UpdateGraphNode(e);
         }
     }
 
@@ -123,4 +123,96 @@ public partial class MainWindow : Window
         // Refresh the graph to reflect changes
         gViewer.Refresh();
     }
+
+    private void GViewer_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+    {
+        // Determine the object under the mouse cursor
+        var objectUnderMouseCursor = gViewer.ObjectUnderMouseCursor;
+
+        if (objectUnderMouseCursor != null && objectUnderMouseCursor.DrawingObject is Microsoft.Msagl.Drawing.Node graphNode)
+        {
+            // Get the node ID
+            string nodeId = graphNode.Id;
+
+            // Find the corresponding Node in your data
+            var correspondingNode = Nodes.FirstOrDefault(n => n.Id == nodeId);
+            if (correspondingNode != null)
+            {
+                // Select the node in the DataGrid
+                NodeGrid.SelectedItem = correspondingNode;
+                NodeGrid.ScrollIntoView(correspondingNode);
+
+                // Open the editing dialog
+                EditNodeProperties(correspondingNode);
+            }
+        }
+    }
+
+    private void EditNodeProperties(Node node)
+    {
+        var editWindow = new EditNodeWindow(node, ColorNames);
+        editWindow.Owner = this;
+        var result = editWindow.ShowDialog();
+
+        if (result == true)
+        {
+            // The PropertyChanged event should handle updates
+            // Force refresh if necessary
+            NodeGrid.Items.Refresh();
+        }
+    }
+
+private void Nodes_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.NewItems != null)
+        {
+            foreach (Node node in e.NewItems)
+            {
+                node.PropertyChanged += Node_PropertyChanged;
+            }
+        }
+        if (e.OldItems != null)
+        {
+            foreach (Node node in e.OldItems)
+            {
+                node.PropertyChanged -= Node_PropertyChanged;
+            }
+        }
+    }
+
+    private void Node_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (!Dispatcher.CheckAccess())
+        {
+            Dispatcher.Invoke(() => Node_PropertyChanged(sender, e));
+            return;
+        }
+
+        var editedNode = sender as Node;
+        if (editedNode == null) return;
+
+        if (gViewer == null || gViewer.Graph == null)
+            return;
+
+        var graphNode = gViewer.Graph.FindNode(editedNode.Id);
+        if (graphNode != null)
+        {
+            if (e.PropertyName == nameof(Node.ColorName) && editedNode.IsClassLeaf)
+            {
+                graphNode.Attr.FillColor = new ColorList().GetColorByName(editedNode.ColorName);
+            }
+
+            if (e.PropertyName == nameof(Node.Label))
+            {
+                graphNode.LabelText = editedNode.Label;
+
+                // Wymuszenie pełnego przeliczenia układu grafu
+                gViewer.Graph = gViewer.Graph;
+            }
+
+            gViewer.Refresh();
+        }
+    }
+
+
 }
